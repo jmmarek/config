240928ad8e284f09903f3e02fa62106ecfae06da
Józek <jozek@debian-asus>
2025-04-22 16:18:08 +0200

    add init script

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo apt install waybar neovim
+mkdir ~/.config/waybar
+wget https://raw.githubusercontent.com/mubin6th/MinimalSwayFX/refs/heads/main/waybar/config.jsonc -P ~/.config/waybar/
+wget https://raw.githubusercontent.com/mubin6th/MinimalSwayFX/refs/heads/main/waybar/style.css -P ~/.config/waybar/
